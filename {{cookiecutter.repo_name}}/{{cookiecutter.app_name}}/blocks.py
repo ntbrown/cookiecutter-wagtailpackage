abeb6c4f988a5ffc76288da4b7ca7a37591941a5
unknown <overflow2341313@gmail.com>
2019-05-21 11:24:32 -0500

    Refactor

--- {{cookiecutter.repo_name}}/{{cookiecutter.app_name}}/blocks.py
+++ {{cookiecutter.repo_name}}/{{cookiecutter.app_name}}/blocks.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
+{% if cookiecutter.blocks != "Comma-separated list of custom Wagtail blocks" %}
 from wagtail.core import blocks
 
 {% for block in cookiecutter.blocks.split(',') %}
